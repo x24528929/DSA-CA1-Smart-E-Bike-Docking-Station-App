8c2b92b908b29eb929c274ec4e4a3c088be70faa
martin <x24528929@student.ncirl.ie>
2026-03-18 05:17:38 +0000

    Simple gui w/ basic nav

---
+++ SmartBikeParkingApp/src/smartbikeparkingapp/SmartBikeParkingApp.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package smartbikeparkingapp;
+
+/**
+ *
+ * @author marti
+ */
+public class SmartBikeParkingApp {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        IndexGUI myGUI = new IndexGUI();
+        myGUI.setVisible(true);
+    }
+    
+}
